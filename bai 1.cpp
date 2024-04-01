2f0f53cf8d3c4b9389751a576520b504a3e4fbe4
thahhtung <104338174+thahhtung@users.noreply.github.com>
2024-04-01 08:05:48 +0700

    bai 1.cpp

---
+++ bai 1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void printMultiplesOfSeven() {
+    for (int i = 10; i <= 99; i++) {
+        if (i % 7 == 0) {
+            printf("%d ", i);
+        }
+    }
+}
+
+int main() {
+    printf("Cac so nguyen co 2 chu so va la boi cua 7 la:\n");
+    printMultiplesOfSeven();
+    return 0;
+}
